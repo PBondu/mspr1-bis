1558742c86c89042c2f189aaa070c7afa36890c8
PBondu <paul.bondu@proton.me>
2024-05-01 13:30:08 +0200

    modif taille flèche

--- src/components/billeterie.js
+++ src/components/billeterie.js
@@ -5,7 +5,7 @@ export default function Billeterie() {
   return (
     <div className="flex flex-col justify-around items-center h-fit py-5 bg-blue-500 text-white">
       <a className="flex flex-col justify-center items-start bg-slate-700 w-fit h-fit p-2 text-2xl text-white rounded-xl" href="https://www.ticketmaster.fr/fr/festival">ACHAT BILLETS</a>
-      <img width={50} height={50} className="my-3" src={arrow} alt="" />
+      <img width={30} height={30} className="my-3" src={arrow} alt="" />
       <p className="text-lg text-center px-4">Réservez vite vos places sur notre plateforme partenaire en y accèdant par le lien ci-dessus</p>
     </div>
   )
