2c30830dca7178b9738c1dc67c865be0272e2b00
PBondu <paul.bondu@proton.me>
2024-05-01 13:28:28 +0200

    ajout footer

---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <div className="flex flex-col justify-around items-center bg-blue-400 text-white h-40 py-3">
+      <div className="flex flex-col items-center">
+        <h2>Contactez-nous !</h2>
+        <p>alex.ample@gémail.com</p>
+      </div>
+      <div className="flex flex-col items-center">
+        <p>Zikos festival 2ème édition</p>
+        <p>Paris Bercy</p>
+      </div>
+    </div>
+  );
+}
